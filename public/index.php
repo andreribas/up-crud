fdc2be8327848baa94e493e934d818dde43bf196
André Ribas <andreribas@gmail.com>
2020-11-09 18:22:06 -0300

    Show no results message

--- public/index.php
+++ public/index.php
@@ -8,12 +8,17 @@
 <body>
     <a href="add_game.php">Add new Game</a>
     <h1>Games</h1>
-    <?php foreach (\andreribas\GameRepository::findAll() as $game): ?>
-        <article>
-            <h2><?= $game->name ?><small> - <?= $game->release_year ?></small></h2>
-            <p><?= $game->description ?></p>
-        </article>
-    <?php endforeach; ?>
+    <?php $games = \andreribas\GameRepository::findAll(); ?>
+    <?php if (count($games)): ?>
+        <?php foreach ($games as $game): ?>
+            <article>
+                <h2><?= $game->name ?><small> - <?= $game->release_year ?></small></h2>
+                <p><?= $game->description ?></p>
+            </article>
+        <?php endforeach; ?>
+    <?php else: ?>
+        No game has been added yet
+    <?php endif; ?>
 </body>
 </html>
 
